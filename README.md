42d8872a63380fdedbf7e1bf4ae2b65e9aec293b
Ovidiu Serban <o.serban@imperial.ac.uk>
2019-12-10 16:24:15 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ovejs-client-sdk
 A simple client SDK for OVE
+
+## Initialise the repository
+
+1. Install node and npm: see [link](https://nodejs.org/en/download/package-manager/) for more details
+2. In the current working dir, using the command line:
+
+```bash
+    npm install
+```
+
+## Testing the repository
+
+```bash
+    npm start
+```
+
+The site should be available on [http://localhost:3000/](http://localhost:3000/)
---
+++ public/lib/ove-sdk.js
@@ -0,0 +1,138 @@
+OVE_SDK_CONFIGURED = false;
+
+function _util_setGlobalParam(obj, field, required = false) {
+    window[field] = window[field] || obj.get(field);
+
+    if (required && !window[field]) {
+        console.error(`No value provided for field = ${field}`)
+    } else if (OVE_DEBUG) {
+        console.log(`[OVE SDK] ${field} = ${window[field]}`)
+    }
+}
+
+function autoconfig() {
+    if (!OVE_SDK_CONFIGURED) {
+        let params = new URL(document.location).searchParams;
+
+        _util_setGlobalParam(params, 'OVE_DEBUG', false);
+        _util_setGlobalParam(params, 'OVE_SPACE', true);
+        _util_setGlobalParam(params, 'OVE_CORE', true);
+        _util_setGlobalParam(params, 'OVE_APP_HTML', true);
+        _util_setGlobalParam(params, 'OVE_APP_IMAGES', true);
+
+        window.OVE_SDK_CONFIGURED = true;
+    }
+}
+
+function observatory() {
+    return region.bind(this)(0, 0, 30720, 4320);
+}
+
+function region(x, y, w, h) {
+    autoconfig();
+
+    return {
+        regionBox: {x, y, w, h},
+        background: _background,
+        cleanSpace: _cleanSpace,
+        html: _html,
+        images: _images
+    };
+}
+
+function _html() {
+    return _app.bind(this)(OVE_APP_HTML);
+}
+
+function _images() {
+    return _app.bind(this)(OVE_APP_IMAGES);
+}
+
+
+function _app(appUrl = "") {
+    _validateRegion.bind(this)();
+
+    return {
+        regionBox: this.regionBox,
+        app: appUrl,
+        create: _create,
+        update: _update
+    }
+}
+
+function _background(color = "White") {
+    autoconfig();
+    _validateRegion.bind(this)();
+
+    return $.ajax({
+        type: 'POST', url: `${OVE_CORE}/section`, data: JSON.stringify({
+            "space": OVE_SPACE,
+            "h": this.regionBox.h, "w": this.regionBox.w, "x": this.regionBox.x, "y": this.regionBox.y,
+            "app": {
+                "url": `${OVE_APP_HTML}`,
+                "states": {
+                    "load": {
+                        "url": `${OVE_APP_HTML}/data/background/index.html?background=${params.color}`,
+                        "launchDelay": 0
+                    }
+                }
+            }
+        }), contentType: 'application/json'
+    })
+}
+
+function _cleanSpace() {
+    autoconfig();
+    _validateRegion.bind(this)();
+
+    return $.ajax({url: `${OVE_CORE}/sections?space=${OVE_SPACE}`, type: 'DELETE'});
+}
+
+function _create(url = "") {
+    _validateRegion.bind(this)();
+    _validateApp.bind(this)();
+
+    console.log("Creating section url =", url, " app =", this.app);
+
+    return _ajax_post({
+        "space": OVE_SPACE,
+        "h": this.regionBox.h, "w": this.regionBox.w, "x": this.regionBox.x, "y": this.regionBox.y,
+        "app": {"url": this.app, "states": {"load": {"url": url}}}
+    });
+}
+
+function _update(url = "") {
+    _validateRegion.bind(this)();
+    _validateApp.bind(this)();
+
+    return _ajax_post({
+        "space": OVE_SPACE,
+        "h": this.regionBox.h, "w": this.regionBox.w, "x": this.regionBox.x, "y": this.regionBox.y,
+        "app": {"url": this.app, "states": {"load": {"url": url}}}
+    });
+}
+
+// helper methods
+
+function _validateRegion() {
+    if (!this.regionBox) {
+        console.error("Region is not defined. Have you called this function directly?")
+    }
+}
+
+function _validateApp() {
+    if (!this.app) {
+        console.error("App is not defined. Have you called this function directly?")
+    }
+}
+
+function _ajax_post(data) {
+    autoconfig();
+
+    return $.ajax({
+        type: 'POST',
+        url: `${OVE_CORE}/section`,
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    });
+}
