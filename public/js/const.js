0d09e0f929b4df8649a11a91ca0a5972f3140690
Ovidiu Serban <o.serban@imperial.ac.uk>
2020-01-16 14:31:22 +0000

    Changes during the testing session

---
+++ public/js/const.js
@@ -0,0 +1,7 @@
+OVE_DEBUG = true;
+
+OVE_CORE = "http://gdo-students.dsi.ic.ac.uk:9080";
+OVE_SPACE = "DOCluster";
+OVE_APP_HTML = "http://gdo-students.dsi.ic.ac.uk:9080/app/html";
+OVE_APP_IMAGES = "http://gdo-students.dsi.ic.ac.uk:9080/app/images";
+
